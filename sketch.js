68089070de051aba4d92ea2398ee9f10a576bf4e
raghav775 <74240452+raghav775@users.noreply.github.com>
2021-01-05 00:49:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+
+
+	Engine.run(engine);
+ function keyPressed (){
+	 if (keyCode === UP_ARROW) {
+		 Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
+	 }
+ }
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+ 
+}
+
+
+
